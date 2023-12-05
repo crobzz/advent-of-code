d009cfc1bf3ab8fdaa61ccd5ff5e9b1e4de958ff
crobzz <128390302+crobzz@users.noreply.github.com>
2023-12-04 20:23:24 -0600

    Update solve.py
    added comments

--- aoc_2023/day3/solve.py
+++ aoc_2023/day3/solve.py
@@ -16,7 +16,18 @@ example = \
 import re
 
 def getSymLocations(row):
+    '''
+    Gets locations of symbols from a given row of text. Adds the adjacent indeces
+    into the return list because adjacent numbers are valid.
+    '''
     symbols = "@#$%^&*-=+/"
+    ## List comprehension
+    # # The following for loop is an equivalent statement
+    # symbolLocations = []
+    # for i in range(len(row)):
+    #     letter = row[i]
+    #     if letter in symbols:
+    #         symbolLocations.append(i)
     symbolLocations = [ index for index, ltr in enumerate(row) if ltr in symbols ]
     withAdjacentValues = []
     for location in symbolLocations:
@@ -27,6 +38,10 @@ def getSymLocations(row):
     return withAdjacentValues
     
 def getNumbers(row):
+    '''
+    Retrieves all numbers from a row with a list of their index(es)
+    Returns a list of tuples<int, list[int]> (full number, indexes on the row)
+    '''
     list_nums = []
     digit = None
     digit_locs = []
@@ -47,10 +62,26 @@ def getNumbers(row):
     return list_nums
             
 #rows = example.split('\n')
+# Step 1. Separate input into rows (on the newline character '\n')
 rows = open("input.txt","r").readlines()
 numRows = len(rows)
 print(numRows)
 
+## Approach for Part 1
+## Each row of text has adjacent rows which may contain symbols that make
+## numbers on the current row a valid part number.
+## For a given row, we can get a list of all the numbers and their locations.
+## We can also combine the valid locations on a row where a part number might be
+## according to the symbols in the adjacent rows. For example:
+## prev_row -> .....*....#....
+## curr_row -> ..@...........+
+## next_row -> .......&.......
+## Produces a valid location mapping for the current row:
+## curr_row -> .VVVVVVVVVVV.VV
+## Where 'V' is a valid location for a number.
+## We can use set theory to check if a valid location mapping (partNumberLocations)
+## overlaps with a particular number's location.
+## If so, we add the number to our running sum.
 sum = 0
 for i in range(len(rows)):
     partNumberLocations = []
@@ -67,9 +98,22 @@ for i in range(len(rows)):
 print(f"Part1: {sum}")
 
 def getGears(row):
+    '''
+    Returns the index for any * symbols on a given row of text.
+    '''
     symbols = "*"
     return [ index for index, ltr in enumerate(row) if ltr in symbols ]
-    
+
+## Approach for part 2.
+## For part 2, we only have to care about '*' gears.
+## So we need to make the gears our focus. So this time, instead of 
+## looping over the number locations, we will loop over the gears
+## first. Then, for any numbers in a row that a gear might affect,
+## we check again with set theory for an overlap with the gear.
+##
+## The real trick to this puzzle is understanding the scope of each 
+## symbol, which can only affect its direct neighbors.
+
 sum = 0
 for i in range(len(rows)):
     partNumberLocations = []
